c9db0bbabcbf770c3b9c59123aa9dbe72dd2d4a0
Sayed-formationmode83 <mejbrisayed7@gmail.com>
2023-02-17 16:37:09 +0100

    merci romain

--- main.js
+++ main.js
@@ -2,9 +2,19 @@ let form = document.getElementById("form");
 let textInput = document.getElementById("textInput");
 let dateInput = document.getElementById("dateInput");
 let textarea = document.getElementById("textarea");
+let  radio= form.choix
+console.log('radio => ', radio);
+
 let msg = document.getElementById("msg");
-let tasks = document.getElementById("tasks");
+// DOM 3 container
+let tasks1 = document.getElementById("tasks1");
+let tasks2 = document.getElementById("tasks2");
+let tasks3 = document.getElementById("tasks3");
+let current = document.getElementById("current");
 let add = document.getElementById("add");
+
+
+
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -29,36 +39,59 @@ form.addEventListener("submit", (e) => {
   };
 
   let data = [];
+  let datacurrent=[];
 
 let acceptData = () => {
   data.push({
     text: textInput.value,
     date: dateInput.value,
     description: textarea.value,  
+    statut: radio.value ,
+
   });
 
   localStorage.setItem("data", JSON.stringify(data));
 
   console.log(data);
-  createTasks();
+  // const btntodo=document.getElementById('TODO');
+  // btntodo.addEventListener("click",createTasks())
+  // const btnCURRENT=document.getElementById('CURRENT');
+  // btntodo.addEventListener("click",createCurrent())
+   createTasks();
 };
 
 let createTasks = () => {
-    tasks.innerHTML = "";
+    tasks1.innerHTML = "";
+    tasks2.innerHTML = "";
+    tasks3.innerHTML = "";
     data.map((x, y) => {
-      return (tasks.innerHTML += `
-      <div id=${y}>
-            <span class="fw-bold">${x.text}</span>
-            <span class="small text-secondary">${x.date}</span>
-            <p>${x.description}</p>
-    
-            <span class="options">
-              <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
-              <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
-            </span>
-          </div>
-      `);
+      let template=`
+<div id=${y}>
+      <span class="fw-bold">${x.text}</span>
+      <span class="small text-secondary">${x.date}</span>
+      <p>${x.description}</p>
+
+      <span class="options">
+        <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+        <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
+      </span>
+    </div>
+`
+      if (x.statut==='faire') {
+        tasks1.innerHTML += template;
+        return tasks1
+      }
+      if (x.statut==='courant') {
+        tasks2.innerHTML += template;
+        return tasks2
+      }
+      if (x.statut==='finie') {
+        tasks3.innerHTML += template;
+        return tasks3
+      }
+
     });
+    
   
     resetForm();
   };
@@ -92,3 +125,27 @@ let createTasks = () => {
     console.log(data);
     createTasks();
   })();
+
+
+  // current
+
+  let createCurrent = () => {
+    current.innerHTML = "";
+    data.map((x, y) => {
+      return (current.innerHTML += `
+      <div id=${y}>
+            <span class="fw-bold">${x.text}</span>
+            <span class="small text-secondary">${x.date}</span>
+            <p>${x.description}</p>
+    
+            <span class="options">
+              <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+              <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
+            </span>
+          </div>
+      `);
+    });
+    
+  
+    resetForm();
+  };
